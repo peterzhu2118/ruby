caf5c498c53e5f3c08450b9fcdbbdd532c6d7fac
Peter Zhu <peter@peterzhu.ca>
2024-02-21 17:53:28 -0500

    Try a fix

--- ext/-test-/load/destruct/destruct.c
+++ ext/-test-/load/destruct/destruct.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
-#include <ruby/ruby.h>
+#include "ruby/ruby.h"
 
 void
 Init_destruct(void)
 {}
 
-void
+RUBY_FUNC_EXPORTED void
 Destruct_destruct(void)
 {
     printf("Calling Destruct_destruct\n");
